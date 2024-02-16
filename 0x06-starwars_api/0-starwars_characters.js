14d045cfb6e0770c16b2d13706b09ecad165d349
Rita2024 <ritaejezie2@gmail.com>
2024-02-16 10:51:46 -0800

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,27 @@
 #!/usr/bin/node
-const axios = require('axios');
-
-async function getMovieCharacters(movieId) {
-  try {
-    const filmResponse = await axios.get(`https://swapi.dev/api/films/${movieId}/`);
-    const charactersUrls = filmResponse.data.characters;
-
-    for (const characterUrl of charactersUrls) {
-      const characterResponse = await axios.get(characterUrl);
-      const characterName = characterResponse.data.name;
-      console.log(characterName);
-    }
-  } catch (error) {
-    console.error(`Error fetching data: ${error.message}`);
-  }
-}
-
-if (process.argv.length !== 3) {
-  console.log('Usage: node script.js <Movie ID>');
-  process.exit(1);
-}
-
+const request = require('request');
 const movieId = process.argv[2];
-getMovieCharacters(movieId);
 
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
